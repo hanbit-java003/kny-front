d63d51215855341abe044a200ca0e0228dfd6688
11 <nayul1013@gmail.com>
2017-07-18 09:23:46 +0900

    update header-menu module

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,4 @@
+
 $('.header-logo').on('click', function() {
     location.href = './';
 });
@@ -5,3 +6,17 @@ $('.header-logo').on('click', function() {
 $('.header-menu-left > li').on('click', function() {
     location.href = './about.html';
 });
+
+var menus = require('./menu');
+
+function initMenu() {
+    var template = require('../template/header-menu.hbs');
+
+    for(var i=0; i<menus.length; i++) {
+        var menuHtml = template(menus[i]);
+
+        $('.header-menu').append(menuHtml);
+    }
+}
+
+initMenu();
---
+++ src/js/menu.js
@@ -0,0 +1,18 @@
+module.exports = [{
+    leftCategory: [{
+        title: 'JUICE&CO.'
+    }, {
+        title: 'PROGRAM'
+    }, {
+        title: 'CLEANSE'
+    }]
+},{
+    rightCategory: [{
+        title: 'DELIVERY'
+    },{
+        title: 'SERVICE'
+    },{
+        title: 'EVENT'
+    }]
+
+}];
