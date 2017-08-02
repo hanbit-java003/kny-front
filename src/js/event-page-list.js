77692484ffe11a454d04762d24fd43592f1de759
11 <nayul1013@gmail.com>
2017-08-02 15:51:34 +0900

    update event page list

---
+++ src/js/event-page-list.js
@@ -0,0 +1,41 @@
+module.exports = [{
+    img: '../img/event/event-board/diet-sm.jpg',
+    text: 'EVENT<br>여름준비! 다이어트 기획전<br>',
+    date: '2017.06.02~2017.07.31'
+},{
+    img: '../img/event/event-board/family-sm.jpg',
+    text: 'EVENT<br>가정의 달 맞이 고객감사 할인 이벤트<br>',
+    date: '2017.05.01~2017.05.31'
+},{
+    img: '../img/event/event-board/spring-sm.jpg',
+    text: 'EVENT<br>봄날의 가벼운 나를 위해<br>',
+    date: '2017.04.01~2017.04.30'
+},{
+    img: '../img/event/event-board/wedding-sm.jpg',
+    text: 'EVENT<br>웨딩클렌즈 이벤트<br>',
+    date: '2017.03.01~2017.03.31'
+},{
+    img: '../img/event/event-board/mobile-sm.jpg',
+    text: 'EVENT<br>모바일 스토어 오픈 이벤트<br>',
+    date: '2017.02.01 ~ 2017.02.28'
+},{
+    img: '../img/event/event-board/holiday-sm.jpg',
+    text: 'EVENT<br>명절 증후군 이벤트<br>',
+    date: '2017.02.01 ~ 2017.02.28'
+},{
+    img: '../img/event/event-board/best-reviewer-sm.jpg',
+    text: 'EVENT<br>베스트 리뷰어를 위한 선물<br>',
+    date: '2017.06.01 ~ 2017.06.30'
+},{
+    img: '../img/event/event-board/new-year20-sm.jpg',
+    text: 'EVENT<br>새해 20% 할인쿠폰 이벤트<br>',
+    date: '2017.02.01 ~ 2017.02.28'
+},{
+    img: '../img/event/event-board/friend-sm.jpg',
+    text: 'EVENT<br>친구에게 주스앤코 소개하면, 10% 할인쿠폰 지급!<br>',
+    date: '상시 이벤트'
+},{
+    img: '../img/event/event-board/new-year-sm.jpg',
+    text: 'EVENT<br>2017년 새해 맞이 증정 이벤트!<br>',
+    date: '2017.01.20 ~ 2017.02.10'
+},];
